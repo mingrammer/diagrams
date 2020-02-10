2552fbfb22feee4eb869efc91c662163ce664d9f
mingrammer <mingrammer@gmail.com>
2020-02-10 22:23:04 +0900

    feat(script:resource): remove bg color suffix

--- scripts/resource.py
+++ scripts/resource.py
@@ -19,6 +19,7 @@ _usage = "Usage: resources.py <cmd> <pvd>"
 def cleaner_aws(f):
     f = f.replace("_", "-")
     f = f.replace("@4x", "")
+    f = f.replace("-light-bg", "")
     for p in cfg.FILE_PREFIXES["aws"]:
         if f.startswith(p):
             f = f[len(p):]
