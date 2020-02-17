28e7b17946143df6792f92cccb67318011772387
S M <>
2020-02-17 13:38:45 -0500

    Add missing module script for OCI provider

---
+++ diagrams/oci/__init__.py
@@ -0,0 +1,12 @@
+"""
+OCI provides a set of services for Oracle Cloud provider.
+"""
+
+from diagrams import Node
+
+
+class _OCI(Node):
+    _provider = "oci"
+    _icon_dir = "resources/oci"
+
+    fontcolor = "#2d3436"
