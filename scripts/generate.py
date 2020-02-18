75033ff1f245baae7c7a4567f1012e3d46715efa
mingrammer <mingrammer@gmail.com>
2020-02-18 21:41:33 +0900

    fix: use if instead of elif

--- scripts/generate.py
+++ scripts/generate.py
@@ -19,7 +19,7 @@ def load_tmpl(tmpl: str) -> Template:
 def up_or_title(pvd: str, s: str) -> str:
     if s in cfg.UPPER_WORDS.get(pvd, ()):
         return s.upper()
-    elif s in cfg.TITLE_WORDS.get(pvd, {}):
+    if s in cfg.TITLE_WORDS.get(pvd, {}):
         return cfg.TITLE_WORDS[pvd][s]
     return s.title()
 
