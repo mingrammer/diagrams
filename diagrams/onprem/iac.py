e3f574f1df72eafc554b3f0a82c34e5e2fed45bb
kreynoldsf5 <k.reynolds@f5.com>
2020-04-18 17:24:16 -0400

    Added Ansible and AWX PNGs.

--- diagrams/onprem/iac.py
+++ diagrams/onprem/iac.py
@@ -8,6 +8,14 @@ class _Iac(_OnPrem):
     _icon_dir = "resources/onprem/iac"
 
 
+class Ansible(_Iac):
+    _icon = "ansible.png"
+
+
+class Awx(_Iac):
+    _icon = "awx.png"
+
+
 class Terraform(_Iac):
     _icon = "terraform.png"
 
