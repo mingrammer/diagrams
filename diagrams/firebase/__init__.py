0ea47509e3d1ad53bfb657396b9277f6c0120913
Leandro Damascena <leandro.damascena@gmail.com>
2020-06-02 13:58:34 +0100

    Fixed firebase init class #184

---
+++ diagrams/firebase/__init__.py
@@ -0,0 +1,12 @@
+"""
+Azure provides a set of services for Microsoft Azure provider.
+"""
+
+from diagrams import Node
+
+
+class _Firebase(Node):
+    _provider = "firebase"
+    _icon_dir = "resources/firebase"
+
+    fontcolor = "#ffffff"
