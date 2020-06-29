01c1656eb7f80146fea2f4730a927cf1fa3d5f85
matthias.gatto <matthias.gatto@matthiasgatto-3.local>
2020-06-29 10:57:16 +0200

    add outscale provider
    Outscale isa French IaaS provider
    
    This commit add support for generated diagram with outscale images
    
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- scripts/resource.py
+++ scripts/resource.py
@@ -109,6 +109,10 @@ def cleaner_elastic(f):
     return f.lower()
 
 
+def cleaner_outscale(f):
+    return f.lower()
+
+
 def cleaner_openstack(f):
     return f.lower()
 
@@ -125,6 +129,7 @@ cleaners = {
     "programming": cleaner_programming,
     "saas": cleaner_saas,
     "elastic": cleaner_elastic,
+    "outscale": cleaner_outscale,
     "generic": cleaner_generic,
     "openstack": cleaner_openstack,
 }
