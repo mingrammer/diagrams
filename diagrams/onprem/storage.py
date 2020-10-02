055c0c2d6640af7c1b284062efc47ba93a413ffa
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2020-10-02 14:23:21 +0200

    Add: Minio storage
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- diagrams/onprem/storage.py
+++ diagrams/onprem/storage.py
@@ -13,6 +13,8 @@ class Ceph(_Storage):
     _icon = "ceph.png"
 class Glusterfs(_Storage):
     _icon = "glusterfs.png"
+class Minio(_Storage):
+    _icon = "minio.png"
 class Openebs(_Storage):
     _icon = "openebs.png"
 class Rook(_Storage):
