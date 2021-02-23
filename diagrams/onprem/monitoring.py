c45d0741e544b2ad5573d637ebe2c6b265901ab0
Mathias Frey <mathias.c.frey@gmail.com>
2021-02-23 09:50:11 +0100

    resized the dynatrace icon

--- diagrams/onprem/monitoring.py
+++ diagrams/onprem/monitoring.py
@@ -15,8 +15,10 @@ class Cortex(_Monitoring):
 class Datadog(_Monitoring):
     _icon = "datadog.png"
 
+
 class Dynatrace(_Monitoring):
     _icon = "dynatrace.png"
+
 
 class Grafana(_Monitoring):
     _icon = "grafana.png"
