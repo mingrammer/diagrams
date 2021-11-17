70f4ce89f7461389999685e260e14d4b9be1de5b
Chris Withers <chris@simplistix.co.uk>
2021-11-17 12:37:19 +0000

    make icon paths relative

--- all_node_types.py
+++ all_node_types.py
@@ -6,8 +6,9 @@ from diagrams import Diagram, Cluster, Edge
 
 
 def main():
+    root = Path(diagrams.__file__).parent
+
     with Diagram("All Node Types", show=False, outformat='svg'):
-        root = Path(diagrams.__file__).parent
         for package_path in sorted(root.iterdir(), reverse=True):
             if package_path.is_dir() and not package_path.name.startswith('__'):
                 package_import = f'{root.name}.{package_path.name}'
@@ -36,6 +37,12 @@ def main():
                                     if prev_node is not None:
                                         node - Edge(style='invis') - prev_node
                                     prev_node = node
+
+    # make the icon paths in the svg relative to the root of the repo:
+    svg_path =  Path('all_node_types.svg')
+    content = svg_path.read_text()
+    svg_path.write_text(content.replace(str(root.parent.absolute()), '.'))
+
 
 if __name__ == '__main__':
     main()
