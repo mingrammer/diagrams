9ed9dfac9a1cb1b9e89daf32914540fd0874d6ad
Akshay Ranjan <akshay.ranjan@gmail.com>
2022-04-30 21:12:40 +0530

    feat(node): Fix some missing files and bump version

---
+++ diagrams/azure/__init__.py
@@ -0,0 +1,14 @@
+
+   
+"""
+Azure provides a set of services for Microsoft Azure provider.
+"""
+
+from diagrams import Node
+
+
+class _Azure(Node):
+    _provider = "azure"
+    _icon_dir = "resources/azure"
+
+    fontcolor = "#ffffff"
