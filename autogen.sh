0a9325f893cdc86503dc2c1e5340ddb8b7434c54
Rémy Léone <rleone@scaleway.com>
2022-06-22 12:04:04 +0200

    add support for Scaleway products

--- autogen.sh
+++ autogen.sh
@@ -3,24 +3,7 @@
 app_root_dir="diagrams"
 
 # NOTE: azure icon set is not latest version
-providers=(
-  "onprem"
-  "aws"
-  "azure"
-  "digitalocean"
-  "gcp"
-  "ibm"
-  "firebase"
-  "k8s"
-  "alibabacloud"
-  "oci"
-  "programming"
-  "saas"
-  "elastic"
-  "generic"
-  "openstack"
-  "outscale"
-)
+providers=("onprem" "aws" "azure" "digitalocean" "gcp" "ibm" "firebase" "k8s" "alibabacloud" "oci" "programming" "saas" "elastic" "generic" "openstack" "outscale" "scaleway")
 
 if ! [ -x "$(command -v round)" ]; then
   echo 'round is not installed'
