b93102d8acdd03138581ecc1e29946fd165fd9ec
Collin Brown <collin.j.brown95@gmail.com>
2022-09-05 19:15:56 -0400

    fix(cheat_sheet.py): change snake_case to PascalCase

--- scripts/cheat_sheet.py
+++ scripts/cheat_sheet.py
@@ -20,7 +20,7 @@ def generate_markdown_table(resource_paths):
         for img_path in resource_paths:
             path = Path(img_path)
             import_path = '.'.join(path.parts[1:-1]).replace("-", "_")
-            module_name = path.parts[-1].replace(".png", "").replace("-", "_")
+            module_name = path.parts[-1].replace("-", " ").title().replace(" ", "").replace(".Png", "")
             table_entry = f"| ![module_name]({path}) | `from {import_path} import {module_name}` |\n"
             md_file.write(table_entry)
 
