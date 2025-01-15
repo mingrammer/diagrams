e10f165d36d48e53b0e3c3ae1ada3e33c47c7d33
BrianLakstins <2887390+BrianLakstins@users.noreply.github.com>
2025-01-15 12:37:37 +0000

    Created example diagram

---
+++ mydiagrams/GroupedWorkersonAWS.py
@@ -0,0 +1,11 @@
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+
+with Diagram("Grouped Workers", show=False, direction="TB"):
+    ELB("lb") >> [EC2("worker1"),
+                  EC2("worker2"),
+                  EC2("worker3"),
+                  EC2("worker4"),
+                  EC2("worker5")] >> RDS("events")
