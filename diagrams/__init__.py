73446bd2509b082f73fb2929484024af225bd151
georgepstaylor <3026030+georgepstaylor@users.noreply.github.com>
2025-10-30 09:55:10 +0000

    Revert "fix: icon image positioning"
    This reverts commit 67e8dbfe3a17b791916e52600e226c6218ebfbbd.

--- diagrams/__init__.py
+++ diagrams/__init__.py
@@ -315,13 +315,14 @@ class Node:
                 self.label = prefix
 
         # fmt: off
-        # If a node has an icon, set the imagepos attr so
-        # that that the icon image is top centered.
+        # If a node has an icon, increase the height slightly to avoid
+        # that label being spanned between icon image and white space.
+        # Increase the height by the number of new lines included in the label.
+        padding = 0.4 * (self.label.count('\n'))
         self._attrs = {
             "shape": "none",
-            "height": str(self._height),
+            "height": str(self._height + padding),
             "image": self._load_icon(),
-            "imagepos": "tc",
         } if self._icon else {}
 
         # fmt: on
